42449b4e6830cdb32adb530d46e6c21cfa031c75
MykolaKushniryk <36865549+MykolaKushniryk@users.noreply.github.com>
2018-07-24 10:14:36 +0300

    Adding_AngularCLI

--- OfferSpace.Web/Controllers/HomeController.cs
+++ OfferSpace.Web/Controllers/HomeController.cs
@@ -1,9 +1,5 @@
 ﻿using OfferSpace.BL.Interfaces;
 using OfferSpace.BL.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace OfferSpace.Web.Controllers
