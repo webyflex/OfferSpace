4b434711dd67bc8a0be23caad00c5750b1a42de5
Manolii Horditsa <paradoxd2l@gmail.com>
2018-07-31 16:33:35 +0300

    AngNotWork

--- OfferSpace.Web/src/app/app.module.ts
+++ OfferSpace.Web/src/app/app.module.ts
@@ -2,18 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
+import { MatCardModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material';
+import { MatDialogModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { UserComponent } from 'src/app/components/user/user.component';
 import { UserRequestComponent } from 'src/app/components/user/request/request.component';
 import { HttpService } from 'src/app/services/http.service';
+import { UserProfileComponent } from './components/user/profile/profile.component';
 
 @NgModule({
   declarations: [
     UserComponent,
-    UserRequestComponent
+    UserRequestComponent,
+    UserProfileComponent
   ],
   imports: [
     HttpClientModule,
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDialogModule
   ],
   providers: [
     HttpService,
--- OfferSpace.Web/src/app/components/user/user.component.ts
+++ OfferSpace.Web/src/app/components/user/user.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { UserProfileComponent } from './profile/profile.component';
 
 @Component({
   selector: 'app-user',
@@ -6,4 +8,9 @@ import { Component } from '@angular/core';
 })
 
 export class UserComponent {
+  constructor(public dialog: MatDialog) { }
+
+  openDialog() {
+    this.dialog.open(UserProfileComponent, {});
+  }
 }
